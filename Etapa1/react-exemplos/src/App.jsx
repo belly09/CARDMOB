f852268a6a9589213e0155f98bc28883fd1419e8
Isabel Fogaça <fogaca.isabel@aluno.ifsp.edu.br>
2025-03-11 09:50:14 -0300

    feat: add Photo Component

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -1,38 +1,55 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Counter from './components/Counter'
+import Counter from './components/Counter';
+import Photo from './components/Photo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
 
-  const updateCount = () => {
-    return count + 1;
-  };
+  const fetchPhotos = async () => {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+      const response = await fetch(url); //Por padrão executa uma request do tipo GET
+      // console.log(response);
+      if (response.status === 200) {
+        const data = await response.json();
+        // console.log(data);
+        const updatedPhotos = data.map( (photo) => ({
+          ...photo,
+          thumbnailUrl: `https://picsum.photos/150?random=${photo.id}`
+        }));
+        setPhotos(updatedPhotos);
+      }
 
-  const updateCount1 = () =>  count + 1;
-  
-  const dados = {
-    "nome": "fulano",
-    // "atualiza": (novo_nome) => $`Novo nome é ${novo_nome}`,
-    "endereco": {
-      "rua": "xyz",
-      "numero": "111",
-      "complementos": ["casa", "na esquina do supermercado ABC"]
+    } catch (error) {
+      console.error('Erro ao buscar fotos', error);
     }
-  };
-  // dados.atualiza("gerson")
-  dados.endereco.complementos[1] // acessando a referência do endereço
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
   return (
     <>
       <Counter  title="Contando..."/>
       <Counter initial="100" />
-      
+      <article>
+        <h1>Album da API</h1>
+        {photos.map( (photo) => (
+          // <article key={photo.id}>
+          //   <h2>ID #{photo.id} {photo.title}</h2>
+          //   <img src={photo.thumbnailUrl} alt={photo.title} />
+          // </article>
+          <Photo photo={photo}/>
+        ) )}
+      </article>
     </>
   )
 }
 
-export default App
+export default App;
